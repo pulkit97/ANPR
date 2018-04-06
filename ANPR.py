a177f0bbbfecb6d799e87e2128014056710b1065
pulkit97 <pulkitkhera97@gmail.com>
2018-04-06 15:23:05 +0530

    Add files via upload

--- ANPR.py
+++ ANPR.py
@@ -11,14 +11,13 @@ import matplotlib.pyplot as plt
 import Preprocess as prep
 import CCA_plate as ccp
 import matplotlib.patches as patches
-import matplotlib.pyplot as plt
 from skimage.filters import threshold_otsu
 import CCA_Segmentation as ccs
 from keras.models import load_model
 import numpy as np
 import Cleaning
 #Reading the data as Greyscale
-img=imread("ImageData\Demo_d.jpg",as_grey=True)
+img=imread("ImageData\Demo_c.jpg",as_grey=True)
 img_obj=[]
 #Displaying the image
 fig,(ax1)=plt.subplots(1)
@@ -45,7 +44,6 @@ for i in plate_like_objects:
     img_obj.append(img[row_start:row_end,col_start:col_end])
 fig.suptitle('Localised Objects Using CCA', fontsize=14, fontweight='bold')
 
-#Yet to apply filtering methods so hard coded
 
 plate_coord=Cleaning.clean_more(img,plate_like_objects)
 row_start,col_start,row_end,col_end=plate_coord
@@ -74,16 +72,15 @@ ax1.imshow(characters[7],cmap="gray")'''
 
 #Prediction
 
-model = load_model('Neural_Net_Models/CNN.h5')
+model = load_model('Neural_Net_Models/CNN_full.h5')
 characters=np.reshape(characters,(len(characters),28,28,1))
 
 
 
 asci=47
 diction={1: '0', 2: '1', 3: '2', 4: '3', 5: '4', 6: '5', 7: '6', 8: '7', 9: '8', 10: '9', 11: 'A', 12: 'B', 13: 'C',
- 14: 'D',15: 'E', 16: 'F', 17: 'G', 18: 'H', 19: 'I', 20: 'J', 21: 'K', 22: 'L', 23: 'M', 24: 'N', 25: 'O', 26: 'P', 27: 'Q', 28: 'R',
- 29: 'S',30: 'T',31: 'U', 32: 'V', 33: 'W', 34: 'X', 35: 'Y',
- 36: 'Z'}
+ 14: 'D',15: 'E', 16: 'F', 17: 'G', 18: 'H', 19: 'I', 20: 'J', 21: 'K', 22: 'L', 23: 'M', 24: 'N', 25: 'O', 26: 'P', 
+ 27: 'Q', 28: 'R', 29: 'S',30: 'T',31: 'U', 32: 'V', 33: 'W', 34: 'X', 35: 'Y', 36: 'Z'}
 
 out=model.predict_classes(characters)
 out_str=""
@@ -98,5 +95,4 @@ rightplate_string=''
 for each in char_order:
     rightplate_string += out_str[column_list_copy.index(each)]
 print(rightplate_string)
-
         
